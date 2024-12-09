e6c64153a0829a5f41a2e49ea9ae5ed7b4f69e0f
Salman-S-Rahman <salmanmern@gmail.com>
2024-12-09 01:22:23 +0100

    react chart used

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useEffect, useState, PureComponent } from "react";
 import { Helmet } from "react-helmet-async";
+import Container from "./Container";
+import {
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+} from "recharts";
 
 const Statistics = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <div>
-      <Helmet>
-        <title>GadgetHeaven | Statistics</title>
-      </Helmet>
-      <h1 className="text-4xl text-red-600">Statistics</h1>
+      <Container>
+        <Helmet>
+          <title>GadgetHeaven | Statistics</title>
+        </Helmet>
+        <h1 className="text-4xl text-red-600">Statistics</h1>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={products}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey={products.product_title} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" background={{ fill: "#eee" }} />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
+      </Container>
     </div>
   );
 };
